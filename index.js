b6abf075ba3d53203889744caa2c8257217113cd
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2024-03-12 16:40:34 -0400

    Update index.js
    Added writefile function

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 const filesystem = require('./node_modules/graceful-fs/graceful-fs')
 const inquirer = require("inquirer");
-const {Circle, Square, Triangle} = require("./assets/shapes");
+const { Circle, Square, Triangle } = require("./assets/shapes");
 
 const questions = [
-    {
-        type: "input",
-        name: "text",
-        message: "TEXT: 3 characters or less.",
-    },
-    {
-        type: "input",
-        name: "text-color",
-        message: "TEXT COLOR: Enter a color OR a hexadecimal:",
-    },
-    {
-        type: "input",
-        name: "shape",
-        message: "SHAPE COLOR: Enter a color keyword OR a hexadecimal:",
-    },
-    {
-        type: "list",
-        name: "pixel-image",
-        message: "Choose which shape you would like",
-        choices: ["Circle", "Square", "Triangle"],
-    },
+	{
+		type: "input",
+		name: "text",
+		message: "TEXT: 3 characters or less.",
+	},
+	{
+		type: "input",
+		name: "text-color",
+		message: "TEXT COLOR: Enter a color OR a hexadecimal:",
+	},
+	{
+		type: "input",
+		name: "shape",
+		message: "SHAPE COLOR: Enter a color keyword OR a hexadecimal:",
+	},
+	{
+		type: "list",
+		name: "pixel-image",
+		message: "Choose which shape you would like",
+		choices: ["Circle", "Square", "Triangle"],
+	},
 ];
 
 async function init() {
-    console.log("Starting Program");
-    let logoText;
-    let textColor;
-    let shapeColor;
-    let shape;
+	console.log("Starting Program");
+	let logoText;
+	let textColor;
+	let shapeColor;
+	let shape;
 
-    const answers = await inquirer.prompt(questions);
+	const answers = await inquirer.prompt(questions);
 
-    // Text Set up 
+	// Text Set up 
 	var user_text = "";
 
-    // Check if text is valid
+	// Check if text is valid
 	console.log(answers.text.length);
 	if (answers.text.length > 0 && answers.text.length <= 3) {
 		logoText = answers.text;
 	} else {
 		console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less");
-        return;
+		return;
 	}
 
 	// Shape setup
@@ -61,11 +61,21 @@ async function init() {
 		console.log("Invalid shape!");
 	}
 
-    // Set shapes color
+	// Set shapes color
 	shape.setColor(shapeColor);
 
 	// Create a new Svg instance and add the shape and text elements to it
-    console.log("Program Exit");
+	console.log("Program Exit");
+}
+
+const writeToFile = (fileName, data) => {
+	console.log(`Writing ${data} to file ${fileName}`)
+	filesystem.writeFile(fileName, data, function (err) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log("Logo saved as logo.svg");
+	});
 }
 
 init();
