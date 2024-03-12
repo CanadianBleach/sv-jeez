f3d5dff9bc2c03f6d1d9d67755e5a050be331b07
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2024-03-12 17:07:50 -0400

    Final Functionality

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const questions = [
 	},
 	{
 		type: "input",
-		name: "text-color",
+		name: "textColor",
 		message: "TEXT COLOR: Enter a color OR a hexadecimal:",
 	},
 	{
 		type: "input",
-		name: "shape",
+		name: "shapeColor",
 		message: "SHAPE COLOR: Enter a color keyword OR a hexadecimal:",
 	},
 	{
 		type: "list",
-		name: "pixel-image",
+		name: "shape",
 		message: "Choose which shape you would like",
 		choices: ["Circle", "Square", "Triangle"],
 	},
@@ -28,15 +28,14 @@ const questions = [
 
 const init = async () => {
 	console.log("Starting Program");
-	let logoText;
-	let textColor;
-	let shapeColor;
-	let shape;
 
 	const answers = await inquirer.prompt(questions);
 
-	// Text Set up 
-	var user_text = "";
+	// Logo Variables
+	let logoText;
+	let textColor = answers.textColor;
+	let shapeColor = answers.shapeColor;
+	let shape;
 
 	// Check if text is valid
 	console.log(answers.text.length);
@@ -48,21 +47,37 @@ const init = async () => {
 	}
 
 	// Shape setup
-	if (user_shape_type.toLowerCase() === "square") {
+	if (answers.shape.toLowerCase() === "square") {
+		console.log("Square");
 		shape = new Square();
 	}
-	else if (user_shape_type.toLowerCase() === "Circle") {
+	else if (answers.shape.toLowerCase() === "circle") {
+		console.log("Circle");
 		shape = new Circle();
 	}
-	else if (user_shape_type.toLowerCase() === "Triangle") {
+	else if (answers.shape.toLowerCase() === "triangle") {
+		console.log("Triangle");
 		shape = new Triangle();
 	}
 	else {
 		console.log("Invalid shape!");
+		return;
 	}
 
 	// Set shapes color
 	shape.setColor(shapeColor);
+
+	let logo = new Svg();
+	logo.setText(logoText, textColor);
+	logo.setShape(shape);
+	let svgString = logo.render();
+
+	//Print shape to log
+	console.log("Displaying shape:\n\n" + svgString);
+
+	console.log("Shape generation complete!");
+	console.log("Writing shape to file...");
+	writeToFile("logo.svg", svgString);
 
 	// Create a new Svg instance and add the shape and text elements to it
 	console.log("Program Exit");
@@ -77,5 +92,25 @@ const writeToFile = (fileName, data) => {
 		console.log("Logo saved as logo.svg");
 	});
 }
+
+class Svg {
+	constructor() {
+		this.textElement = ''
+		this.shapeElement = ''
+	}
+	render() {
+		console.log("REDERING", this.textElement)
+		return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+	}
+	setText(text, color) {
+		this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+	}
+	setShape(shape) {
+		this.shapeElement = shape.render()
+
+	}
+}
+
+module.export = { Svg };
 
 init();
